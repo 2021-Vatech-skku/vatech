035705d8cc6b713eb74e10491eb7b1305f71763d
LeeJunSeok <wt358@naver.com>
2021-07-23 12:34:43 +0900

    shell script and sql file are modified

--- mapreduces/bee_sql.sql
+++ mapreduces/bee_sql.sql
@@ -6,5 +6,13 @@ show tables;
 create or replace TABLE treatments
   USING DELTA
   LOCATION '/Users/leejunseok/work/vtshare/mapreduces/treatments';
+create or replace TABLE patients
+  USING DELTA
+  LOCATION '/Users/leejunseok/work/vtshare/mapreduces/patients';
+create or replace TABLE receipt
+  USING DELTA
+  LOCATION '/Users/leejunseok/work/vtshare/mapreduces/receipts';
+
+
 
 show tables;
--- mapreduces/submit-spark-thrift.sh
+++ mapreduces/submit-spark-thrift.sh
@@ -5,13 +5,11 @@ DELTALAKE_VERSION=${DELTALAKE_VERSION:-1.0.0}
 SPARK_SQL_KAFKA_VERSION=${SPARK_SQL_KAFKA_VERSION:-3.1.2}
 MONGODB_CONNECTOR_VERSION=${MONGODB_CONNECTOR_VERSION:-3.0.1}
 
-$SPARK_HOME/sbin/start-thriftserver.sh --packages org.apache.hadoop:hadoop-aws:${HADOOP_VERSION},io.delta:delta-core_2.12:${DELTALAKE_VERSION},org.apache.spark:spark-sql-kafka-0-10_2.12:${SPARK_SQL_KAFKA_VERSION},org.mongodb.spark:mongo-spark-connector_2.12:${MONGODB_CONNECTOR_VERSION}\
-    $@
+$SPARK_HOME/sbin/start-thriftserver.sh --packages org.apache.hadoop:hadoop-aws:${HADOOP_VERSION},io.delta:delta-core_2.12:${DELTALAKE_VERSION},org.apache.spark:spark-sql-kafka-0-10_2.12:${SPARK_SQL_KAFKA_VERSION},org.mongodb.spark:mongo-spark-connector_2.12:${MONGODB_CONNECTOR_VERSION}
 
 beeline \
     -u jdbc:hive2://localhost:10000\
     -n haruband\
     -p haru1004\
     -f bee_sql.sql\
-    $@
 
