74ef69beeec61b0b9daf01d8625702bf665a3803
Baek, Song Hyun <cmcm0012@gmail.com>
2021-07-26 18:01:05 +0900

    etl code

---
+++ pipeline/chart-etl.py
@@ -0,0 +1,53 @@
+import os
+import json
+from pyspark import SparkContext, SQLContext
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+from pyspark.sql.types import *
+from pyspark.sql.functions import from_json
+
+
+sc = SparkConf()
+sc.set(
+    "spark.hadoop.fs.s3a.access.key",
+    os.environ.get("MINIO_ACCESS_KEY", "haruband"),
+)
+sc.set(
+    "spark.hadoop.fs.s3a.secret.key",
+    os.environ.get("MINIO_SECRET_KEY", "haru1004"),
+)
+sc.set("spark.hadoop.fs.s3a.path.style.access", "true")
+sc.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc.set("spark.hadoop.fs.s3a.endpoint", "https://minio.develop.vsmart00.com")
+
+spark = SparkSession.builder.appName("Chart ETL").config(conf=sc).getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+
+df = spark.read.format("json").load("s3a://mongodb/topics/clever.dev0-chart")
+df = df.filter(df.operationType == "insert").select("fullDocument")
+schema = StructType(
+    [
+      StructField(
+          "_id", StructType([StructField("$oid", StringType(), False)]), False
+      ),
+      StructField("content", StructType([
+        StructField("tx", StructType([
+          StructField("treatments", ArrayType(StructType([
+            StructField("treats", ArrayType(StructType([
+              StructField("name", StringType(), False),
+              StructField("price", IntegerType(), False)
+            ])),False)
+          ])), False)
+        ]), False)
+      ]), False),
+      StructField("hospitalId", StringType(), False),
+      StructField("date", StructType([StructField("$date", IntegerType(), False)]), False),
+      StructField("patient", StringType(), False)
+    ]
+)
+df = df.withColumn("test", from_json(df.fullDocument, schema))
+df.select("test").show(10, truncate=False)
+df.printSchema()
+
+# sc.set("spark.hadoop.fs.s3a.endpoint", "http://minio.it.vsmart00.com")
+df.write.format("delta").conf("spark.hadoop.fs.s3a.endpoint", "http://minio.it.vsmart00.com").save("s3a://songhyun/chart")
